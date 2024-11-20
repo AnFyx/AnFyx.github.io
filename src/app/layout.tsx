e7004bb10337e1de1309ab1a9d87280ec8df3d7b
AnFyx <thomas.iliot@isen.yncrea.fr>
2024-11-20 22:25:00 +0100

    Page Profile stylisée

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import MobileNav from "@/components/MobileNav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}>
+        {children}
+        <MobileNav></MobileNav>
+      </body>
     </html>
   );
 }
